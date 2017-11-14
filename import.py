45b3fbaf37c874d4e1926f63a065d8e597468ef5
Stew <ghost.pizza@gmail.com>
2017-11-14 23:58:22 +0100

    add import.py, test_import.csv and README.md

---
+++ import.py
@@ -0,0 +1,32 @@
+import csv,sys,os
+project_dir = 'C: \\Utenti\\Stew\Desktop\\root'
+
+#sys.patch.append(project_dir)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'progetto1.settings'
+
+import django
+django.setup()
+
+from app_libri.models import Book
+
+data = csv.reader(
+	open('test_import.csv'), delimiter=",")
+
+for row in data:
+    if row[0] != 'ASIN':
+        post = Book()
+        post.asin = row[0]
+        post.titolo = row[1]
+        post.autore = row[2]
+        post.descrizione = row[4]
+        post.valutazione = row[6]
+        post.prezzo = row[7]
+        post.link = row[21]
+        post.image = row[22]
+        post.save()
+
+       
+       
+        
+
+    
