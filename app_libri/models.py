2e7b0e20695423bdea9352dd13fc0fb17150530b
Karim N Gorjux <karimone@gmail.com>
2017-11-17 17:48:38 +1100

    Fix problem with the admin

--- app_libri/models.py
+++ app_libri/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+"""
+blank=True significa che accetta il valore "vuoto" che viene salvato dall'admin.
+null=True e' relativo al database e significa che accetta il "non valore"
+Purtroppo in Django il model e l'admin sono molto legati quando l'admin e' in realta' solo 
+un presentation layer e non dovrebbe avere niente a che fare con la definizione dei modelli
+"""
+
 class Book(models.Model):
     asin = models.CharField(max_length=250)
     titolo = models.CharField(max_length=250)
@@ -8,10 +15,10 @@ class Book(models.Model):
     descrizione = models.TextField() 
     valutazione = models.CharField(max_length=10)  
     prezzo = models.CharField(max_length=10)
-    link = models.URLField()
-    image = models.URLField()
+    link = models.URLField(null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
     data = models.DateTimeField(auto_now=False, auto_now_add=True)
-    slug = models.SlugField()
+    slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.titolo
